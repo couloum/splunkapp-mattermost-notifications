fa8779ce93884dbcd32fa4d59b44c9e8ca8d7774
haraksin <george.haraksin@laverne.edu>
2020-04-17 12:32:56 -0700

    Adding attachment test

--- bin/mattermost.py
+++ bin/mattermost.py
@@ -3,6 +3,8 @@ import json
 import urllib2
 import gzip
 import csv
+import datetime
+import os
 
 from fnmatch import fnmatch
 
@@ -13,10 +15,39 @@ def create_markdown_separator(value_list):
     markdown_string = "|"
     for value in value_list:
         markdown_string = '-|'
-    print >> sys.stderr "DEBUG Markdown header length is %s" %s str(value_list)
+    print >> sys.stderr, "DEBUG Markdown header length is %s" % str(value_list)
     return markdown_string
 
-def send_notification(msg, url):
+def create_mm_field(title, value, short=None):
+    field_dict = {
+        "title": title,
+        "value": value
+    }
+    if short:
+        field_dict['short'] = short
+    return field_dict
+
+def create_attachment_dict(fallback, pretext, text, title, author_name, **kwargs):
+    attachment_dict = {
+        "fallback": fallback,
+        "pretext": pretext,
+        "text": text,
+        "title": title,
+        "author_name": author_name
+    }
+    field_list = []
+
+    for param, value in kwargs:
+        print >> sys.stderr, "DEBUG Filtering arg %s" % param
+        if '_field' in param:
+            print >> sys.stderr, "DEBUG Adding arg %s to field list" % param
+            field_list.append(value)
+
+    attachment_dict['fields'] = field_list
+
+    return attachment_dict
+    
+def send_notification(msg, url, attachment=None):
     print >> sys.stderr, "INFO Sending message to Mattermost url %s" % (url)
     msg_limit = 10000
     if len(msg) > msg_limit:
@@ -30,6 +61,12 @@ def send_notification(msg, url):
 
     body = json.dumps(data)
     print >> sys.stderr, 'DEBUG Calling url="%s" with body=%s' % (url, body)
+
+    if attachment:
+        data['attachments'] = [attachment]
+        print >> sys.stderr, 'DEBUG Adding attachment to body with attachment=%s' % (url, body)
+        body = json.dumps(data)
+    
     req = urllib2.Request(url, body, {"Content-Type": "application/json"})
     try:
         res = urllib2.urlopen(req)
@@ -48,10 +85,13 @@ def table_broker(payload):
     table = settings.get('table')
     msg = settings.get('message')
     url = settings.get('url')
-    return_value = send_notification(msg, url)
+    return_value = False
     if table:
+        print >> sys.stderr, "DEBUG Results found"
         results_file_location = settings.get('results_file')
-        print >> sys.stderr, "INFO Results table at %s" % results_file_location
+        print >> sys.stderr, "INFO Results at %s" % results_file_location
+
+        results_string = ""
         with gzip.open(results_file_location, 'rb') as results_file:
             results = csv.reader(results_file)
             header_line = next(results)
@@ -59,13 +99,75 @@ def table_broker(payload):
             results_string = create_markdown_string(header_line)
             results_string = results_string + create_markdown_separator(header_line)
             results_string = [create_markdown_string(line) for line in data]
+
+        # Decide whether to send this info via table or attachment
+        if table == "table":
+            return_value = send_notification(msg, url)
+            print >> sys.stderr, "INFO Results table selected"
+
             table_return_value = send_notification(results_string, url)
-            if not success:
+            if not table_return_value:
                 print >> sys.stderr, "FATAL Failed trying to send Mattermost table"
                 sys.exit(2)
             else:
                 print >> sys.stderr, "INFO Mattermost table successfully sent"
 
+        elif table == "attach":
+            print >> sys.stderr, "INFO Results attachment selected"
+            saved_search_name = settings.get('search_name')
+            print >> sys.stderr, "DEBUG Saved search name: %s" % saved_search_name
+            results_link = settings.get('results_link')
+            print >> sys.stderr, "DEBUG Results link: %s" % results_link
+            owner = settings.get('owner')
+            author_name = owner
+            print >> sys.stderr, "DEBUG Search owner: %s" % owner
+            app = settings.get('app')
+            print >> sys.stderr, "DEBUG Search app context: %s" % App
+            count = settings.get('count')
+            print >> sys.stderr, "DEBUG Search result count: %s" % count
+            trigger_time = settings.get('trigger_time')
+            print >> sys.stderr, "DEBUG Trigger time: %s" % trigger_time
+            trigger_date = settings.get('trigger_date')
+            print >> sys.stderr, "DEBUG Trigger date: %s" % trigger_date
+            trigger_epoch = settings.get('trigger_epoch')
+            print >> sys.stderr, "DEBUG Trigger epoch: %s" % trigger_epoch
+            trigger_epoch_value = datetime.fromtimestamp(trigger_epoch)
+            trigger_epoch_string = trigger_epoch_string.strftime('%Y-%m-%d %H:%M:%S')
+            print >> sys.stderr, "DEBUG Trigger epoch string: %s" % trigger_epoch_string
+            description = settings.get('description')
+            print >> sys.stderr, "DEBUG Search description: %s" % description
+
+            fallback = "Results generated by alert \"%s\"" % saved_search_name
+            pretext = "Results in CSV format. Search results in Splunk can be found [here](%s)." % results_link
+            text = data
+            title = "%s results" %s saved_search_name
+            app_field = create_mm_field("App", app, short=True)
+            search_field = create_mm_field("Saved Search", saved_search_name, short=True)
+            description_field = create_mm_field("Description", description, short=False)
+            owner_field = create_mm_field("Owner", owner, short=True)
+            count_field = create_mm_field("Results Count", count, short=True)
+            results_field = create_mm_field("Results Link", results_link, short=False)
+            date_field = create_mm_field("Date Alerted", trigger_epoch_string, short=True)
+            
+            print >> sys.stderr, "DEBUG Creating attachment dictionary"
+            attachment_dict = create_attachment_dict(
+                fallback,
+                pretext,
+                text,
+                title,
+                author_name,
+                app_field,
+                search_field,
+                description_field,
+                owner_field,
+                count_field,
+                results_field,
+                date_field
+            )
+            
+
+    else:
+        print >> sys.stderr, "WARN Results table not found"
     return return_value
 
 if __name__ == "__main__":
